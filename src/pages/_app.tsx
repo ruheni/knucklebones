f102ef0b8844adec2266bd06b5f042bf09e58f3c
Federico Viotti <fedeviotti@gmail.com>
2023-07-19 23:25:26 +0200

    chore: add usage of base theme and poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {ChakraProvider} from "@chakra-ui/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import baseTheme from "~/themes/baseTheme";
+import {Poppins} from "~/components/fonts/Poppins";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,7 +13,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <ChakraProvider>
+      <ChakraProvider theme={baseTheme}>
+        <Poppins />
         <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
