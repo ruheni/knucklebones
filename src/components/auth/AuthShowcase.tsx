78ffc1e07b935aed3771c25c3324f8a3cbc9896d
Federico Viotti <fedeviotti@gmail.com>
2023-07-19 22:54:27 +0200

    refactor: move AuthShowcase component

---
+++ src/components/auth/AuthShowcase.tsx
@@ -0,0 +1,26 @@
+import {signIn, signOut, useSession} from "next-auth/react";
+import {api} from "~/utils/api";
+import {Flex, Text, Button} from "@chakra-ui/react";
+
+export function AuthShowcase() {
+    const { data: sessionData } = useSession();
+
+    const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+        undefined, // no input
+        { enabled: sessionData?.user !== undefined }
+    );
+
+    return (
+        <Flex direction="column" justifyContent="center" alignItems="center">
+            <Text>
+                {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+                {secretMessage && <span> - {secretMessage}</span>}
+            </Text>
+            <Button
+                onClick={sessionData ? () => void signOut() : () => void signIn()}
+            >
+                {sessionData ? "Sign out" : "Sign in"}
+            </Button>
+        </Flex>
+    );
+}
