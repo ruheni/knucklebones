550072456e9f25db86c5954c9d9c5323dadaac5b
Federico Viotti <fedeviotti@gmail.com>
2023-07-19 23:25:54 +0200

    chore: edit home wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
 import {Flex, Heading, Text} from "@chakra-ui/react";
-import {AuthShowcase} from "~/components/auth/AuthShowcase";
+import {Menu} from "~/features/home/Menu";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -13,12 +13,13 @@ export default function Home() {
         <meta name="description" content="Knucklebones game" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex h="100vh" direction="column" justifyContent="center" alignItems="center" gap={8}>
+      <Flex h="100vh" direction="column" justifyContent="center" alignItems="center" gap={8} m={8}>
+        <Menu />
         <Heading>Knucklebones</Heading>
-        <Text>
-          {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-        </Text>
-        <AuthShowcase />
+        {/*<Text>*/}
+        {/*  {hello.data ? hello.data.greeting : "Loading tRPC query..."}*/}
+        {/*</Text>*/}
+        {/*<AuthShowcase />*/}
       </Flex>
     </>
   );
