7a33211bde69f54d19a3acf48641a39e50a5fdf6
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 07:08:00 +0200

    refactor: remove Menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 // import { api } from "~/utils/api";
 import {Flex, Heading} from "@chakra-ui/react";
-import {Menu} from "~/features/home/Menu";
 
 export default function Home() {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -14,7 +13,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Flex h="100vh" direction="column" alignItems="center" gap={8} m={8}>
-        <Menu />
         <Heading>Knucklebones</Heading>
         {/*<Text>*/}
         {/*  {hello.data ? hello.data.greeting : "Loading tRPC query..."}*/}
