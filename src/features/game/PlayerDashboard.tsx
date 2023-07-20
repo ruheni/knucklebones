761f2c2132a87d43acd5f7bf7281bc40aad4dcc3
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 18:05:25 +0200

    chore: edit PlayerDashboard component

--- src/features/game/PlayerDashboard.tsx
+++ src/features/game/PlayerDashboard.tsx
@@ -1,1 +1,36 @@
-export const PlayerDashboard = ({name}: {name: string}) => <div>Player Dashboard {name}</div>;
+import {Heading, Text, Flex} from "@chakra-ui/react";
+import React from "react";
+import {DiceBoard} from "~/features/game/DiceBoard";
+import {PartialScore} from "~/features/game/PartialScore";
+
+interface Props {
+    name: string;
+    score: number;
+    position?: "left" | "right";
+    values: number[];
+}
+
+export const PlayerDashboard = ({name, score, position = "left", values}: Props) => (
+    <Flex justifyContent="center" alignItems="center">
+        <Flex direction="column" width="300px" maxWidth="300px">
+            {position === "left" && (
+                <>
+                    <Heading>{name}</Heading>
+                    <Text>Score: {score}</Text>
+                </>
+            )}
+        </Flex>
+        <Flex direction={position === "left" ? "column-reverse" : "column"}>
+            <PartialScore values={values} />
+            <DiceBoard values={values} />
+        </Flex>
+        <Flex direction="column" width="300px" maxWidth="300px">
+            { position === "right" && (
+                <>
+                    <Heading>{name}</Heading>
+                    <Text>Score: {score}</Text>
+                </>
+            )}
+        </Flex>
+    </Flex>
+);
