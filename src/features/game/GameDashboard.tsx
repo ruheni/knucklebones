467936054a9efee02b8da7d4153be2549e28e4c8
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 15:46:45 +0200

    chore: add GameDashboard

---
+++ src/features/game/GameDashboard.tsx
@@ -0,0 +1,17 @@
+import {Box, Button, Text, Stack, Flex} from "@chakra-ui/react";
+import {type Players} from "~/pages";
+
+interface Props {
+    players: Players;
+    onQuit: () => void;
+}
+export const GameDashboard = ({onQuit, players}: Props) => {
+    return (
+        <Stack bg={"red.100"}>
+            <Button onClick={onQuit} alignSelf="start">Quit game</Button>
+            <Text>Game dashboard</Text>
+            <Box>{players.playerOne}</Box>
+            <Box>{players.playerTwo}</Box>
+        </Stack>
+    );
+}
