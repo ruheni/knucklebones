550f66f038cf8cbb2d6e740beb5bece2d44fdf7e
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 18:04:21 +0200

    chore: add Die component

---
+++ src/features/game/Die.tsx
@@ -0,0 +1,17 @@
+import {Box, Flex} from "@chakra-ui/react";
+
+interface Props {
+    value: number;
+}
+export const Die = ({value}: Props) => {
+    return (
+        <Flex
+            width="50px"
+            height="50px"
+            bg="primary.100"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="md"
+        >{value > 0 ? value : null}</Flex>
+    );
+}
