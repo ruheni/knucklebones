1c5489213111187eb19ebaf4a0468d258e469eee
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 10:29:42 +0200

    feat: add navbar logic

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,12 +1,32 @@
-import {Flex} from "@chakra-ui/react";
+import {Flex, Link} from "@chakra-ui/react";
 import ActiveLink from "~/components/ActiveLink";
+import {useRouter} from "next/router";
+import NextLink from "next/link";
 
 export default function Navbar() {
+    const router = useRouter();
+    console.log("router", router);
+
+    const getContent = () => {
+        if (router.asPath === "/game"){
+            return (
+                <Link as={NextLink} href="/" style={{textDecoration: "none"}}>
+                    Quit game
+                </Link>
+            );
+        }
+        return (
+            <>
+                <ActiveLink href="/">Play</ActiveLink>
+                <ActiveLink href="/how-to-play">How to play</ActiveLink>
+                <ActiveLink href="/ranking">Ranking</ActiveLink>
+            </>
+        )
+    }
+
     return (
         <Flex w="100%" justifyContent="space-between" p={8}>
-            <ActiveLink href="/">Play</ActiveLink>
-            <ActiveLink href="/how-to-play">How to play</ActiveLink>
-            <ActiveLink href="/ranking">Ranking</ActiveLink>
+            {getContent()}
         </Flex>
     )
 }
