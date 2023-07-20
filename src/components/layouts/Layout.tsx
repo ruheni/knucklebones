bcad0bfa732746974d0b0925afeb60e061f5dae9
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 10:08:04 +0200

    chore: edit Layout component

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,5 +1,8 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import {Box, Flex} from "@chakra-ui/react";
+import * as React from "react";
+import {FOOTER_HEIGHT, NAVBAR_HEIGHT, PAGE_WIDTH} from "~/constants/layout";
 
 interface Props {
     children: React.ReactNode;
@@ -9,7 +12,14 @@ export default function Layout({ children }: Props) {
     return (
         <>
             <Navbar />
-            <main>{children}</main>
+            <Flex
+                direction="column"
+                alignItems="center"
+                py="32px"
+                minHeight={`calc(100vh - ${NAVBAR_HEIGHT}px - ${FOOTER_HEIGHT}px)`}
+            >
+                <Box width={PAGE_WIDTH}>{children}</Box>
+            </Flex>
             <Footer />
         </>
     )
