0a3396ca990f5fe0aefa5212807f958324d947b9
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 07:07:41 +0200

    chore: create ActiveLink

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router'
+import {Link} from "@chakra-ui/react";
+import NextLink from "next/link";
+
+interface Props {
+    children: React.ReactNode,
+    href: string,
+}
+function ActiveLink({ children, href }: Props) {
+    const router = useRouter()
+    const style = {
+        marginRight: 10,
+        color: router.asPath === href ? '#AF09F4' : 'black',
+    }
+
+    return (
+        <Link as={NextLink} href={href} style={style}>
+            {children}
+        </Link>
+    )
+}
+
+export default ActiveLink
