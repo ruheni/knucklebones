8300b834f73cac0cc0bc977ada02dc16740ed687
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 07:07:33 +0200

    chore: create Footer

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,9 @@
+import {Flex} from "@chakra-ui/react";
+
+export default function Navbar() {
+    return (
+        <Flex w="100%" bg="violet">
+            Made with ❤️ by <a href="https://github.com/fedeviotti">@fedeviotti</a>
+        </Flex>
+    )
+}
