7ec362a50130246a583826bca9e0c93853b96238
Federico Viotti <fedeviotti@gmail.com>
2023-07-20 10:08:54 +0200

    refactor: edit Footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import {Flex} from "@chakra-ui/react";
 
 export default function Navbar() {
     return (
-        <Flex w="100%" bg="violet">
+        <Flex w="100%" p={8} justifyContent="center">
             Made with ❤️ by <a href="https://github.com/fedeviotti">@fedeviotti</a>
         </Flex>
     )
