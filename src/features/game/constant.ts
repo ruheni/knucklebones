aab371e1662aa05546162216c3e5d89db6fe12d7
Federico Viotti <fedeviotti@gmail.com>
2023-07-24 15:08:17 +0200

    refactor: change PARTIAL_SCORE_MAP type

--- src/features/game/constant.ts
+++ src/features/game/constant.ts
@@ -1,5 +1,7 @@
-export const PARTIAL_SCORE_MAP: Record<number, number[]> = {
-  0: [0, 3, 6],
-  1: [1, 4, 7],
-  2: [2, 5, 8],
-};
+// This constant maps the cell indexes for each column
+// i.e. column 0: includes cells 0, 3 and 6
+export const PARTIAL_SCORE_MAP: number[][] = [
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
