2b809f1efaa1afba241eb50bff4f4f80c5aa1394
Federico Viotti <fedeviotti@gmail.com>
2023-07-24 17:25:58 +0200

    chore: add simple grid

--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -1,5 +1,5 @@
 import {
-  Heading, Stack,
+  Heading, Stack, Table, TableContainer, Tbody, Td, Th, Thead, Tr,
 } from "@chakra-ui/react";
 import * as React from "react";
 import Head from "next/head";
@@ -13,7 +13,32 @@ const Ranking = () => (
     </Head>
     <Stack spacing={16}>
       <Heading alignSelf="center">Ranking</Heading>
-
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>#</Th>
+              <Th>Player</Th>
+              <Th isNumeric>Score</Th>
+              <Th />
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>1</Td>
+              <Td>Noa</Td>
+              <Td isNumeric>140</Td>
+              <Td textAlign="end">History</Td>
+            </Tr>
+            <Tr>
+              <Td>2</Td>
+              <Td>Teo</Td>
+              <Td isNumeric>130</Td>
+              <Td textAlign="end">History</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Stack>
   </>
 );
