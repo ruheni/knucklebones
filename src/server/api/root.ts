6b5680ce53795674333ff1faaf34911ab110b7bc
Federico Viotti <fedeviotti@gmail.com>
2023-07-24 18:43:52 +0200

    chore: add game router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { gameRouter } from "~/server/api/routers/game";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  game: gameRouter,
 });
 
 // export type definition of API
