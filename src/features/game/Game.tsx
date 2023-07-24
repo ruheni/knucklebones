757745af88a46c98e673fcdeea555fa875d7eb7e
Federico Viotti <fedeviotti@gmail.com>
2023-07-24 18:44:24 +0200

    chore: start game mutation

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -3,10 +3,21 @@ import type { GameFormValues } from "~/features/game/GameForm";
 import { GameForm } from "~/features/game/GameForm";
 import { GameBoard } from "~/features/game/GameBoard";
 import * as React from "react";
+import { api } from "~/utils/api";
 
 export const Game = () => {
   const { state: { players }, dispatch } = useGame();
-  const onSubmitHandler = (values: GameFormValues) => {
+  const startGame = api.game.startGame.useMutation({
+    onSuccess: (data) => {
+      console.log("data", data);
+      // save data.id on the context to pass it to addMove.mutate
+    },
+  });
+  const onSubmitHandler = async (values: GameFormValues) => {
+    startGame.mutate({
+      player: values.playerOne,
+      opponent: values.playerTwo,
+    });
     dispatch({ type: "addPlayers", payload: values });
   };
 
