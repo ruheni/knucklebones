ac92abe349ade36c62379288cc363f38494dad88
Federico Viotti <fedeviotti@gmail.com>
2023-07-24 16:31:45 +0200

    chore: add animation

--- src/features/game/GameBoard.tsx
+++ src/features/game/GameBoard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {
-  Button,
+  Box,
+  Button, chakra,
   Divider,
   Modal,
   ModalBody,
@@ -15,10 +16,12 @@ import {
 } from "@chakra-ui/react";
 import { PlayerBoard } from "~/features/game/PlayerBoard";
 import { useGameOver } from "~/features/game/hooks/useGameOver";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface Props {
   onQuit: () => void;
 }
+
 export const GameBoard = ({ onQuit }: Props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { isGameOver, winnerName } = useGameOver();
@@ -29,15 +32,10 @@ export const GameBoard = ({ onQuit }: Props) => {
     }
   }, [isGameOver, onOpen]);
 
-  const onCloseHandler = () => {
-    onClose();
-    onQuit();
-  };
-
   return (
     <>
       <Stack direction="column" spacing={12}>
-        <Button onClick={onQuit} alignSelf="start">Quit game</Button>
+        <Button onClick={onQuit} alignSelf="center" colorScheme={isGameOver ? "primary" : "default"}>Quit game</Button>
         <Stack direction="column" spacing={4} alignItems="center">
           <PlayerBoard
             playerNumber={0}
@@ -48,26 +46,39 @@ export const GameBoard = ({ onQuit }: Props) => {
           />
         </Stack>
       </Stack>
-      {isOpen && (
-        <Modal isOpen={isOpen} onClose={onCloseHandler}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Game over</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <Text fontWeight="bold">
-                {`${winnerName} wins!`}
-              </Text>
-            </ModalBody>
+      <AnimatePresence>
+        {isOpen && (
+        <Box
+          key="modal"
+          as={motion.div}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: 0.2 } }}
+          exit={{ opacity: 0 }}
+        >
+          <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Game over</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Text fontWeight="bold">
+                  {`${winnerName} wins!`}
+                </Text>
+              </ModalBody>
 
-            <ModalFooter>
-              <Button colorScheme="primary" mr={3} onClick={onCloseHandler}>
-                Close
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      )}
+              <ModalFooter>
+                <Button colorScheme="primary" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Box>
+        )}
+      </AnimatePresence>
     </>
   );
 };
