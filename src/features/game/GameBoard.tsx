cb3c24710fa89563b430a6200871ca3e79020959
Federico Viotti <fedeviotti@gmail.com>
2023-07-24 15:36:11 +0200

    chore: add modal on game over

--- src/features/game/GameBoard.tsx
+++ src/features/game/GameBoard.tsx
@@ -1,21 +1,73 @@
 import * as React from "react";
-import { Button, Divider, Stack } from "@chakra-ui/react";
+import {
+  Button,
+  Divider,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { PlayerBoard } from "~/features/game/PlayerBoard";
+import { useGameOver } from "~/features/game/hooks/useGameOver";
 
 interface Props {
   onQuit: () => void;
 }
-export const GameBoard = ({ onQuit }: Props) => (
-  <Stack direction="column" spacing={12}>
-    <Button onClick={onQuit} alignSelf="start">Quit game</Button>
-    <Stack direction="column" spacing={4} alignItems="center">
-      <PlayerBoard
-        playerNumber={0}
-      />
-      <Divider />
-      <PlayerBoard
-        playerNumber={1}
-      />
-    </Stack>
-  </Stack>
-);
+export const GameBoard = ({ onQuit }: Props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isGameOver, winnerName } = useGameOver();
+
+  React.useEffect(() => {
+    if (isGameOver) {
+      onOpen();
+    }
+  }, [isGameOver, onOpen]);
+
+  const onCloseHandler = () => {
+    onClose();
+    onQuit();
+  };
+
+  return (
+    <>
+      <Stack direction="column" spacing={12}>
+        <Button onClick={onQuit} alignSelf="start">Quit game</Button>
+        <Stack direction="column" spacing={4} alignItems="center">
+          <PlayerBoard
+            playerNumber={0}
+          />
+          <Divider />
+          <PlayerBoard
+            playerNumber={1}
+          />
+        </Stack>
+      </Stack>
+      {isOpen && (
+        <Modal isOpen={isOpen} onClose={onCloseHandler}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Game over</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text fontWeight="bold">
+                {`${winnerName} wins!`}
+              </Text>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="primary" mr={3} onClick={onCloseHandler}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      )}
+    </>
+  );
+};
