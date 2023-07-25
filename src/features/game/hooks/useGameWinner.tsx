2e7c9f06ee642c43b86524fd0f7eddd1b5aaa301
Federico Viotti <fedeviotti@gmail.com>
2023-07-25 14:03:40 +0200

    refactor: useGameWinner

--- src/features/game/hooks/useGameWinner.tsx
+++ src/features/game/hooks/useGameWinner.tsx
@@ -1,13 +1,22 @@
 import { usePlayerTotalScore } from "~/features/game/hooks/usePlayerTotalScore";
 import { useGame } from "~/features/game/GameContext";
+import { useGameOver } from "~/features/game/hooks/useGameOver";
 
 export const useGameWinner = () => {
   const { state: { players } } = useGame();
+  const { isGameOver } = useGameOver();
   const playerScore = usePlayerTotalScore({ playerNumber: 0 });
   const opponentScore = usePlayerTotalScore({ playerNumber: 1 });
 
+  if (isGameOver) {
+    return {
+      winnerName: (playerScore > opponentScore ? players[0]?.name : players[1]?.name) || "Player",
+      score: playerScore > opponentScore ? playerScore : opponentScore,
+    };
+  }
+
   return {
-    winnerName: (playerScore > opponentScore ? players[0]?.name : players[1]?.name) || "Player",
-    score: playerScore > opponentScore ? playerScore : opponentScore,
+    winnerName: "",
+    score: 0,
   };
 };
