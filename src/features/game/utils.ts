723311158badd18ae7fd8bc21e99e75f80963bc7
Federico Viotti <fedeviotti@gmail.com>
2023-07-25 11:24:02 +0200

    chore: create utils function to calculate next values state

--- src/features/game/utils.ts
+++ src/features/game/utils.ts
@@ -1,1 +1,34 @@
-export const getColumnByIndex = (index: number) => index % 3;
+const getColumnByIndex = (index: number) => index % 3;
+
+type CalculatePlayerUpdatedValuesProps = {
+  playerValues: number[];
+  valueToPlace: number;
+  position: number;
+};
+export const calculatePlayerUpdatedValues = ({
+  playerValues,
+  valueToPlace,
+  position,
+}: CalculatePlayerUpdatedValuesProps) => [
+  ...playerValues.slice(0, position),
+  valueToPlace,
+  ...playerValues.slice(position + 1),
+];
+
+type CalculateOpponentUpdatedValuesProps = {
+  opponentValues: number[];
+  valueToPlace: number;
+  position: number;
+};
+export const calculateOpponentUpdatedValues = ({
+  opponentValues,
+  valueToPlace,
+  position,
+}: CalculateOpponentUpdatedValuesProps) => [
+  ...opponentValues.map((value, i) => {
+    if (getColumnByIndex(i) === getColumnByIndex(position)) {
+      return (value === valueToPlace ? 0 : value);
+    }
+    return value;
+  }),
+];
