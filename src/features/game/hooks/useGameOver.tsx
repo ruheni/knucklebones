bba015b2b69ed639ee2855cada05038d07d080e5
Federico Viotti <fedeviotti@gmail.com>
2023-07-25 13:07:05 +0200

    chore: edit useGameOver hook

--- src/features/game/hooks/useGameOver.tsx
+++ src/features/game/hooks/useGameOver.tsx
@@ -1,14 +1,9 @@
 import { useGame } from "~/features/game/GameContext";
-import { usePlayerTotalScore } from "~/features/game/hooks/usePlayerTotalScore";
 
 export const useGameOver = () => {
   const { state: { players } } = useGame();
-  const scorePlayerOne = usePlayerTotalScore({ playerNumber: 0 });
-  const scorePlayerTwo = usePlayerTotalScore({ playerNumber: 1 });
-  const winnerName = scorePlayerOne > scorePlayerTwo ? players[0]?.name : players[1]?.name;
 
   return {
     isGameOver: players.some((player) => player.values.every((value) => value !== 0)),
-    winnerName,
   };
 };
