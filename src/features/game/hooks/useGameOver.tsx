e5453eb217be98a5833f886f340668f9d74e69e4
Federico Viotti <fedeviotti@gmail.com>
2023-07-25 14:03:20 +0200

    chore: add game id on useGameOver hook

--- src/features/game/hooks/useGameOver.tsx
+++ src/features/game/hooks/useGameOver.tsx
@@ -1,9 +1,10 @@
 import { useGame } from "~/features/game/GameContext";
 
 export const useGameOver = () => {
-  const { state: { players } } = useGame();
+  const { state: { players, gameId } } = useGame();
 
   return {
+    gameId,
     isGameOver: players.some((player) => player.values.every((value) => value !== 0)),
   };
 };
