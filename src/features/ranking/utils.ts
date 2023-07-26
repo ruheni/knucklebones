1c48e0ef182193c32066432934e5ddb49c42157b
Federico Viotti <fedeviotti@gmail.com>
2023-07-26 16:35:32 +0200

    chore: add formatValues function

---
+++ src/features/ranking/utils.ts
@@ -0,0 +1,12 @@
+export const formatValues = (values: string) => {
+  const valuesArray = values.split(",");
+  return valuesArray.map((e, i) => {
+    if ((i + 1) % 3) {
+      return `${e},`;
+    }
+    if (i + 1 === valuesArray.length) {
+      return e;
+    }
+    return `${e}|`;
+  }).join("");
+};
