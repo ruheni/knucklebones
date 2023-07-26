53d04b910f3d8ff55b4687adc49dde7effc7410a
Federico Viotti <fedeviotti@gmail.com>
2023-07-26 16:37:56 +0200

    refactor: usePlayerPartialScore hook

--- src/features/game/hooks/usePlayerPartialScore.tsx
+++ src/features/game/hooks/usePlayerPartialScore.tsx
@@ -1,5 +1,5 @@
-import { PARTIAL_SCORE_MAP } from "~/features/game/constant";
 import { useGame } from "~/features/game/GameContext";
+import { calculatePartialScore } from "~/features/game/utils/calculatePartialScore";
 
 type Props = {
   playerNumber: number;
@@ -7,31 +7,9 @@ type Props = {
 
 export const usePlayerPartialScore = ({ playerNumber }: Props) => {
   const { state: { players } } = useGame();
+  const values = players[playerNumber]?.values || [];
 
-  const result = PARTIAL_SCORE_MAP.reduce((
-    acc,
-    columnCells,
-    columnIndex,
-  ) => {
-    const values = players[playerNumber]?.values || [];
-    return {
-      ...acc,
-      [`column${columnIndex + 1}Score`]: columnCells.reduce((
-        accumulator,
-        cellIndex,
-      ) => {
-        const occurrences = values
-          .filter((v, i) => v !== 0 && columnCells.includes(i))
-          .filter((v) => v === values[cellIndex])
-          .length || 1;
-        return accumulator + ((values[cellIndex] || 0) * occurrences);
-      }, 0),
-    };
-  }, ({
-    column1Score: 0,
-    column2Score: 0,
-    column3Score: 0,
-  }));
+  const partialScores = calculatePartialScore({ values });
 
-  return result;
+  return partialScores;
 };
