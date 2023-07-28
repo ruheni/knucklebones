839a598182f1274bcdac42ff46115f46af16f470
Federico Viotti <fedeviotti@gmail.com>
2023-07-28 18:15:43 +0200

    refactor(game): add css die for the dice board

--- src/features/game/components/Die.tsx
+++ src/features/game/components/Die.tsx
@@ -1,21 +1,37 @@
-import { Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import type { PropsWithChildren } from "react";
+import styles from "./die.module.css";
+
+const Pip = () => <span className={styles.pip} />;
+
+const Face = ({ children, cursor, onClick }: PropsWithChildren<{ cursor: "grab" | "not-allowed"; onClick?: () => void }>) => (
+  <Box
+    onClick={onClick}
+    cursor={cursor}
+    className={styles.face}
+  >
+    {children}
+  </Box>
+);
 
 type Props = {
   value: number;
-  onClick: () => void;
-  isClickable: boolean;
+  onClick?: () => void;
+  isClickable?: boolean;
 };
-export const Die = ({ value, onClick, isClickable }: Props) => (
-  <Flex
-    width="50px"
-    height="50px"
-    bg="primary.100"
-    alignItems="center"
-    justifyContent="center"
-    borderRadius="md"
-    onClick={onClick}
-    cursor={value === 0 && isClickable ? "grab" : "not-allowed"}
-  >
-    {value > 0 ? value : null}
-  </Flex>
-);
+
+export const Die = ({ value, onClick, isClickable }: Props) => {
+  const pips = Number.isInteger(value)
+    ? Array(value)
+      .fill(0)
+      .map(() => <Pip key={crypto.randomUUID()} />)
+    : null;
+  return (
+    <Face
+      onClick={onClick}
+      cursor={value === 0 && isClickable ? "grab" : "not-allowed"}
+    >
+      {pips}
+    </Face>
+  );
+};
