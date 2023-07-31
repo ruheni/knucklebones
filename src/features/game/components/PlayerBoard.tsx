c270648ada65ebbb818a4d5252cc975f768c53b8
Federico Viotti <fedeviotti@gmail.com>
2023-07-31 12:41:11 +0200

    fix(game): add PlayerBoard mobile version

--- src/features/game/components/PlayerBoard.tsx
+++ src/features/game/components/PlayerBoard.tsx
@@ -1,4 +1,6 @@
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import {
+  Box, Flex, Stack, useMediaQuery,
+} from "@chakra-ui/react";
 import React from "react";
 import { DiceBoard } from "~/features/game/components/DiceBoard";
 import { PartialScore } from "~/features/game/components/PartialScore";
@@ -11,15 +13,37 @@ type Props = {
 
 export const PlayerBoard = ({
   playerOrder,
-}: Props) => (
-  <Stack direction={playerOrder === "player" ? "row-reverse" : "row"} spacing={12} width="900px">
-    <Box width="400px" />
-    <Flex direction={playerOrder === "player" ? "column-reverse" : "column"} width="300px">
-      <PartialScore playerOrder={playerOrder} />
-      <DiceBoard playerOrder={playerOrder} />
-    </Flex>
-    <PlayerConsole
-      playerOrder={playerOrder}
-    />
-  </Stack>
-);
+}: Props) => {
+  const [isDesktop] = useMediaQuery("(min-width: 768px)");
+
+  if (!isDesktop) {
+    return (
+      <Stack direction={playerOrder === "player" ? "column-reverse" : "column"} spacing={12} alignItems="center">
+        <Flex direction={playerOrder === "player" ? "column-reverse" : "column"} alignItems="center">
+          <PartialScore playerOrder={playerOrder} />
+          <DiceBoard playerOrder={playerOrder} />
+        </Flex>
+        <PlayerConsole
+          playerOrder={playerOrder}
+        />
+      </Stack>
+    );
+  }
+
+  return (
+    <Stack
+      direction={playerOrder === "player" ? "row-reverse" : "row"}
+      spacing={12}
+      justifyContent={playerOrder === "player" ? "start" : "end"}
+    >
+      <Flex minWidth={[null, "200px"]} />
+      <Flex direction={playerOrder === "player" ? "column-reverse" : "column"} grow={1}>
+        <PartialScore playerOrder={playerOrder} />
+        <DiceBoard playerOrder={playerOrder} />
+      </Flex>
+      <PlayerConsole
+        playerOrder={playerOrder}
+      />
+    </Stack>
+  );
+};
